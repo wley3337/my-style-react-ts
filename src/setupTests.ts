7e7e08531d5ef16d2dd5987887a7fe4856fec968
wley3337 <wley3337@gmail.com>
2020-01-28 16:48:26 -0500

    Update BASE_URL for nested API routes
    Also updated package.json to remove extraneous jest setup instructions.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -5,11 +5,11 @@
 import "@testing-library/jest-dom/extend-expect";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-const localStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  removeItem: jest.fn(),
-  clear: jest.fn()
-};
-global.localStorage = localStorageMock;
+// const localStorageMock = {
+//   getItem: jest.fn(),
+//   setItem: jest.fn(),
+//   removeItem: jest.fn(),
+//   clear: jest.fn()
+// };
+// global.localStorage = localStorageMock;
 configure({ adapter: new Adapter() });
