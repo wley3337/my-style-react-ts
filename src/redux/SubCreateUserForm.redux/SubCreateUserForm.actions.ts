a0c90de8c26c6f6ad3db87d94c27a6860472b66c
wley3337 <wley3337@gmail.com>
2020-01-30 09:14:29 -0500

    Cleanup leftover comments

--- src/redux/SubCreateUserForm.redux/SubCreateUserForm.actions.ts
+++ src/redux/SubCreateUserForm.redux/SubCreateUserForm.actions.ts
@@ -4,6 +4,7 @@ import {
   ISubCreateUserForm,
   ISubCreateUserFormAction
 } from "./SubCreateUserForm.types";
+import { BASE_URL, setUser } from "../actions";
 
 export const subCreateUserForm = (
   subCreateUserFormObj: ISubCreateUserForm
@@ -15,4 +16,27 @@ export function* watchSubCreateUserForm() {
   yield takeEvery(SUB_CREATE_USER_FORM, handleSubCreateUserForm);
 }
 
-export function* handleSubCreateUserForm(action: ISubCreateUserFormAction) {}
+export function* handleSubCreateUserForm(action: ISubCreateUserFormAction) {
+  const { createUserForm, history } = action.payload;
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({ user: createUserForm })
+  };
+
+  try {
+    const res = yield call(fetch, BASE_URL + "/users", options);
+    const resObj = yield call([res, "json"]);
+
+    if (resObj.success) {
+      yield put(setUser(resObj.user));
+      localStorage.setItem("myStyle-Token", resObj.token);
+      history.push("/dashboard");
+    }
+  } catch (err) {
+    console.error("SubCreateUserForm: ", err);
+  }
+}
