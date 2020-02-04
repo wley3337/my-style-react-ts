84a86f60133f847f8897e92f4ee1ef9b29080a67
wley3337 <wley3337@gmail.com>
2020-02-04 16:38:29 -0500

    Cleanup unneeded imports in Landing.tsx

--- src/containers/Landing/Landing.tsx
+++ src/containers/Landing/Landing.tsx
@@ -4,29 +4,25 @@ import { RouteComponentProps } from "react-router-dom";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import CreateUserForm from "../../components/CreateUserForm/CreateUserForm";
 import { connect } from "react-redux";
-import * as actions from "../../redux/actions";
 import { AppState } from "../../redux/rootReducer";
 import { IUser } from "../../redux/User.redux/User.types";
-import { History } from "history";
 import AutoLogin from "../../components/AutoLogin/AutoLogin";
 interface ILandingProps extends RouteComponentProps {
-  //actions
-  autoLogin: (history: History) => void;
   //state
   user: IUser;
 }
 
-export const Landing: React.FC<ILandingProps> = ({
-  user,
-  autoLogin,
-  history
-}) => {
+export const Landing: React.FC<ILandingProps> = ({ user }) => {
   return (
     <div>
-      {localStorage.getItem("myStyle-Token") && !user.username ? null : (
-        <Route exact path="/" render={p => <AutoLogin {...p} />} />
+      {localStorage.getItem("myStyle-Token") ? (
+        user.username === "" ? (
+          <Route path="/" render={p => <AutoLogin {...p} />} />
+        ) : null
+      ) : (
+        <Route exact path="/" render={p => <LoginForm {...p} />} />
       )}
-      <Route exact path="/" render={p => <LoginForm {...p} />} />
+
       <Route
         exact
         path="/create-account"
@@ -40,4 +36,4 @@ const mSTP = (state: AppState) => {
   return { user: state.user };
 };
 
-export default connect(mSTP, actions)(Landing);
+export default connect(mSTP)(Landing);
