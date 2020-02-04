49316fda09b63721b8127c017f341ab29ca3dfa6
wley3337 <wley3337@gmail.com>
2020-02-04 10:21:33 -0500

    Add AutoLogin Component to process autoLogin

---
+++ src/components/AutoLogin/AutoLogin.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../../redux/actions";
+import { History } from "history";
+import { IAutoLoginAction } from "../../redux/AutoLogin.redux/AutoLogin.types";
+
+interface IAutoLoginProps extends RouteComponentProps {
+  //actions
+  autoLogin: (history: History) => IAutoLoginAction;
+}
+
+export const AutoLogin: React.FC<IAutoLoginProps> = ({
+  history,
+  autoLogin
+}) => {
+  autoLogin(history);
+  return <div>Logging in</div>;
+};
+
+export default connect(null, actions)(AutoLogin);
--- src/containers/Landing/Landing.tsx
+++ src/containers/Landing/Landing.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 import { Route } from "react-router";
+import { RouteComponentProps } from "react-router-dom";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import CreateUserForm from "../../components/CreateUserForm/CreateUserForm";
 import { connect } from "react-redux";
+import * as actions from "../../redux/actions";
 import { AppState } from "../../redux/rootReducer";
 import { IUser } from "../../redux/User.redux/User.types";
-interface ILandingProps {
+import { History } from "history";
+import AutoLogin from "../../components/AutoLogin/AutoLogin";
+interface ILandingProps extends RouteComponentProps {
+  //actions
+  autoLogin: (history: History) => void;
   //state
   user: IUser;
 }
 
-export const Landing: React.FC<ILandingProps> = ({ user }) => {
+export const Landing: React.FC<ILandingProps> = ({
+  user,
+  autoLogin,
+  history
+}) => {
   return (
     <div>
-      {localStorage.getItem("myStyle-Token") && !user.username
-        ? "login"
-        : "already logged int"}
+      {localStorage.getItem("myStyle-Token") && !user.username ? null : (
+        <Route exact path="/" render={p => <AutoLogin {...p} />} />
+      )}
       <Route exact path="/" render={p => <LoginForm {...p} />} />
       <Route
         exact
@@ -30,4 +40,4 @@ const mSTP = (state: AppState) => {
   return { user: state.user };
 };
 
-export default connect(mSTP)(Landing);
+export default connect(mSTP, actions)(Landing);
